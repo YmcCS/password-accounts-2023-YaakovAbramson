1cdca72598e0112efff62ba3dabe0663e4b6e840
Yaakov Abramson <114956127+YaakovAbramson@users.noreply.github.com>
2023-02-20 16:39:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-def create_account():
-    username = input("Enter a username: ")
-    password = input("Enter a password: ")
 
-    with open("account_details.txt", "a") as file:
-        file.write(f"{username},{password}\n")
-    
-for i in range(6):
-    create_account()
-    with open("account_details.txt", "r") as file:
-        print(file.read())
